58d9bcb2d0302f8638e26d7920132b5d3cff30b8
Yassine Hmidi <ge84tef@tum.de>
2025-09-02 14:53:06 +0200

    fixed UI nitpicks

--- src/main/java/de/tum/cit/aet/artemis/core/service/feature/FeatureToggleService.java
+++ src/main/java/de/tum/cit/aet/artemis/core/service/feature/FeatureToggleService.java
@@ -75,9 +75,9 @@ public class FeatureToggleService {
         features = hazelcastInstance.getMap("features");
 
         // Features that are neither enabled nor disabled should be enabled by default
-        // This ensures that all features (except the Science API, TutorSuggestions, and AtlasAgent) are enabled once the system starts up
+        // This ensures that all features (except the Science API and TutorSuggestions) are enabled once the system starts up
         for (Feature feature : Feature.values()) {
-            if (!features.containsKey(feature) && feature != Feature.Science && feature != Feature.TutorSuggestions && feature != Feature.AtlasAgent) {
+            if (!features.containsKey(feature) && feature != Feature.Science && feature != Feature.TutorSuggestions) {
                 features.put(feature, true);
             }
         }
@@ -88,10 +88,6 @@ public class FeatureToggleService {
 
         if (!features.containsKey(Feature.TutorSuggestions)) {
             features.put(Feature.TutorSuggestions, false);
-        }
-
-        if (!features.containsKey(Feature.AtlasAgent)) {
-            features.put(Feature.AtlasAgent, false);
         }
     }
 
